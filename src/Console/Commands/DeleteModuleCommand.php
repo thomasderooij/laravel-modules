331aaa99d3d972316cf0455e13d99d801dcadb28
Thomas de Rooij <thomasderooij@gmail.com>
2020-06-18 05:56:29 +0200

    fuckup fix

--- src/Console/Commands/DeleteModuleCommand.php
+++ src/Console/Commands/DeleteModuleCommand.php
@@ -18,7 +18,7 @@ class DeleteModuleCommand extends ModuleCommand
      *
      * @var string
      */
-    protected $description = 'Delete a module permanently, With all of it\'s code";
+    protected $description = 'Delete a module permanently, With all of it\'s code';
 
     public function __construct (ModuleManager $manager)
     {
