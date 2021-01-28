4e5a6c5fd4d136fd382030a92d2099e95d6c8e7e
Thomas de Rooij <thomasderooij@gmail.com>
2021-01-28 17:44:58 +0100

    removed php 8 from the requirement list because of dependencies

--- src/Contracts/Factories/ConfigFactory.php
+++ src/Contracts/Factories/ConfigFactory.php
@@ -2,8 +2,16 @@
 
 namespace Thomasderooij\LaravelModules\Contracts\Factories;
 
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+
 interface ConfigFactory
 {
+    /**
+     * Create modules config files and metadata files
+     *
+     * @param string $rootDir
+     * @throws FileNotFoundException
+     */
     public function create (string $rootDir) : void;
 
     public function undo () : void;
