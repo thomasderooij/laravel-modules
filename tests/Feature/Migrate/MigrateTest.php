c486c2c5097dad5071e57ab5f92380d1fac8fdd4
Thomas de Rooij <thomasderooij@gmail.com>
2021-03-07 23:47:42 +0100

    added empty functional tests for the migrate command, to be implemented

---
+++ tests/Feature/Migrate/MigrateTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Thomasderooij\LaravelModules\Tests\Feature\Migrate;
+
+use Thomasderooij\LaravelModules\Tests\Test;
+
+class MigrateTest extends Test
+{
+    public function testMigrateWithoutModule () : void
+    {
+
+    }
+
+    public function testMigrateWithModule () : void
+    {
+
+    }
+
+    public function testMigrateWithWorkbench () : void
+    {
+
+    }
+
+    public function testMigrateWithVanillaModule () : void
+    {
+        
+    }
+}
